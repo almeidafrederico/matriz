6de04bbac9858d0c58b234e91deefad04734e684
Frederico S Almeida <almeidaa.frederico@gmail.com>
2022-01-06 18:34:46 -0300

    Add matriz retangulares + scan number and your right legth up down

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,32 +10,60 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		int[][] mat = new int[n][n];
+		int m = sc.nextInt();
+		int[][] mat = new int[n][m];
 		
 		for (int i = 0; i<mat.length; i++) {
-			for(int j = 0; j<mat[1].length; j++) {
+			for(int j = 0; j<mat[i].length; j++) {
 				mat[i][j] = sc.nextInt();
 			}
 		}
 		
+		System.out.println("-----------------------------------------");
 		System.out.print("Main Diagonal: ");
-		for (int i = 0; i<mat.length; i++) {
-			System.out.print(mat[i][i] + ", ");
+		int aux = 0; 
+		if(mat.length >= mat[1].length) {
+			aux = mat[1].length;
+		} else {
+			aux = mat.length;
+		}
+		for (int i = 0; i<aux; i++) {
+				System.out.print(mat[i][i] + ", ");
 		}
 		
+		System.out.println();
+		System.out.println("-----------------------------------------");
 		int count = 0;
 		for (int i = 0; i<mat.length; i++) {
-			for(int j = 0; j<mat[1].length; j++) {
+			for(int j = 0; j<mat[i].length; j++) {
 				if(mat[i][j] < 0 ) {
 					count++;
 				}
 			}
 		}
-		
 		System.out.println ("Negative Numbers = " + count);
 		
-		
-		
+		System.out.println("-----------------------------------------");
+				int number = sc.nextInt();
+		for (int i = 0; i<mat.length; i++) {
+			for(int j = 0; j<mat[i].length; j++) {
+				if(mat[i][j] == number) {
+					System.out.println("Position " + i + ", " + j + ":");
+					if(j>0) {
+						System.out.println("Left: " + mat[i][j-1]);
+					}
+					if(j<mat[i].length-1) {
+						System.out.println("Right: " + mat[i][j+1]);
+					}
+					if(i>0) {
+						System.out.println("Up: " + mat[i-1][j]);
+					}
+					if(i<mat.length-1) {
+						System.out.println("Down: " + mat[i+1][j]);
+					}
+				}				
+			}
+		}
 		sc.close();
 	}
 
