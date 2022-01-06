7f4ad841fc5ad69cae0c621a6d0cd7841e7c70f3
Frederico S Almeida <almeidaa.frederico@gmail.com>
2022-01-06 17:47:24 -0300

    Finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,34 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-
+		int n = sc.nextInt();
+		int[][] mat = new int[n][n];
+		
+		for (int i = 0; i<mat.length; i++) {
+			for(int j = 0; j<mat[1].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.print("Main Diagonal: ");
+		for (int i = 0; i<mat.length; i++) {
+			System.out.print(mat[i][i] + ", ");
+		}
+		
+		int count = 0;
+		for (int i = 0; i<mat.length; i++) {
+			for(int j = 0; j<mat[1].length; j++) {
+				if(mat[i][j] < 0 ) {
+					count++;
+				}
+			}
+		}
+		
+		System.out.println ("Negative Numbers = " + count);
+		
+		
+		
+		sc.close();
 	}
 
 }
